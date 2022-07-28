0da35cca31065fd388da283abd1eb711fe475293
RedZNouggy <81537743+RedZNouggy@users.noreply.github.com>
2022-07-28 10:15:59 +0200

    Create SentinelOne-Profile.ps1

---
+++ SentinelOne-Profile.ps1
@@ -0,0 +1,179 @@
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+
+Write-Host "...................................."                                 -NoNewline -BackgroundColor White -ForegroundColor White 
+Write-Host "++++++++-"                                                            -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta 
+Write-Host ".................................."                                              -BackgroundColor White -ForegroundColor White
+
+Write-Host "..................................."                                  -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-"                                                           -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta 
+Write-Host ".................................."                                              -BackgroundColor White -ForegroundColor White
+
+Write-Host "......................"                                               -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "+=-"                                                                  -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-"                                                           -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta  
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=:"                                                                  -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta  
+Write-Host "....................."                                                           -BackgroundColor White -ForegroundColor White
+
+Write-Host "....................."                                                -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "*#=-"                                                                 -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-"                                                           -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=#+"                                                                 -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "...................."                                                            -BackgroundColor White -ForegroundColor White
+
+Write-Host "..................."                                                  -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-====-"                                                               -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-"                                                           -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".........."                                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-===="                                                                -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..................."                                                             -BackgroundColor White -ForegroundColor White
+
+Write-Host ".................."                                                   -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host ":=====-"                                                              -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "....."                                                                -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-+*=.-========--=*+-"                                                 -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "....."                                                                -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=====-"                                                              -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................."                                                               -BackgroundColor White -ForegroundColor White
+
+Write-Host "................."                                                    -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-======-.-+=#====.-========--====#=+..-======-"                       -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======-.========.-========--========.-======="                      -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "*=======-.========.-========--========.-======#+"                     -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-=======*"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+ 
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-========"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............."                                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========-.========.-========--========.-=======*"                    -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "................"                                                     -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "+#======-.========.-========--========.-======#:"                     -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..............."                                                                 -BackgroundColor White -ForegroundColor White
+
+Write-Host "................."                                                    -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "+======-.========..--------.-========.-======+"                       -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................"                                                                -BackgroundColor White -ForegroundColor White
+
+Write-Host ".................."                                                   -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-*#===-.========...........-========.-===#*-"                         -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "................."                                                               -BackgroundColor White -ForegroundColor White
+
+Write-Host "....................."                                                -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host ":==-.========"                                                        -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..........."                                                          -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-========.-=*-"                                                       -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "...................."                                                            -BackgroundColor White -ForegroundColor White
+
+Write-Host ".........................."                                           -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "========"                                                             -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..........."                                                          -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=======*"                                                            -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "........................."                                                       -BackgroundColor White -ForegroundColor White
+
+Write-Host "............................"                                         -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-*===="                                                               -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..........."                                                          -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-====*-"                                                              -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..........................."                                                     -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................."                                      -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-*="                                                                  -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host "..........."                                                          -NoNewline -BackgroundColor White -ForegroundColor White
+Write-Host "-=*-"                                                                 -NoNewline -BackgroundColor White -ForegroundColor DarkMagenta
+Write-Host ".............................."                                                  -BackgroundColor White -ForegroundColor White
+
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
+Write-Host "..............................................................................." -BackgroundColor White -ForegroundColor White
